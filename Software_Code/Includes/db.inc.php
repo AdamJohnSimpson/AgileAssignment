e5a206f691b97f86a025f17c171010f364f76ac8
Craig <craig.ritchie450@gmail.com>
2021-01-25 16:11:59 +0000

    Added the port into the connection statement

--- Software_Code/Includes/db.inc.php
+++ Software_Code/Includes/db.inc.php
@@ -4,9 +4,10 @@ $servername_db = "mysql80-afe9.euw2.cloud.ametnes.com";
 $username_db = "KTWomToCcQ";
 $password_db = "OFCREfcS96DmPrNOBSpu";
 $name_db = "2191101310";
+$port = 3316;
 
 // Create connection
-$conn = new mysqli($servername_db, $username_db, $password_db, $name_db);
+$conn = new mysqli($servername_db, $username_db, $password_db, $name_db, $port);
 
 // Check connection
 if ($conn->connect_error) {
