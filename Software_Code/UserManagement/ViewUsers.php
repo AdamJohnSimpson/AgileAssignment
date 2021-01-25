ab60ccd7c52be03f414fac7074d9c79d9025bca5
Craig <craig.ritchie450@gmail.com>
2021-01-25 15:21:10 +0000

    Added very rough initial html to create a basic page

--- Software_Code/UserManagement/ViewUsers.php
+++ Software_Code/UserManagement/ViewUsers.php
@@ -1,4 +1,22 @@
 <?php include $_SERVER[ 'DOCUMENT_ROOT' ] ."/includes/db.inc.php";?>
 <html>
-<title>User Management</title>
+	<title>User Management</title>
+	<body>
+		<h1> Manage Users </h1>
+		
+		<div>
+			<button onclick="window.location.href='https://www.google.com';" type="button">Create Account</button>
+			
+			<table>
+				<tr>
+					<th>User</th>
+				</tr>
+				<tr>
+					<td>Bob</td>
+					<td><button onclick="window.location.href='https://www.google.com';" type="button">Manage</button></td>
+				</tr>
+			</table>
+		</div>
+	</body>
+
 </html>
