522da26b4f63a0d9d3c4ea5737f2488edb1f14ba
Craig <craig.ritchie450@gmail.com>
2021-01-26 15:10:49 +0000

    Removed complicated code from login

--- Software_Code/login.php
+++ Software_Code/login.php
@@ -13,7 +13,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 require_once "Includes/db.inc.php";
 
 // Define variables and initialize with empty values
-// $username = $password = "";
+$username = $password = "";
 // $username_err = $password_err = "";
 
 // Processing form data when form is submitted
@@ -36,56 +36,33 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT UserID, UserName, Password FROM Users WHERE UserName = $username";
+        $sql = "SELECT * FROM User WHERE UserName = '$username'";
+        $result = mysqli_query($conn, $sql);
+        $count = 0;
+        while($row = mysqli_fetch_array($result)){
+            $count=1;
+            $id = $row["UserID"];
+            $username = $row["UserName"];
+            $hashed_password = $row["Password"];
 
-        if($stmt = $conn->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam("$username", $param_username, PDO::PARAM_STR);
-
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Check if username exists, if yes then verify password
-                if($stmt->rowCount() == 1){
-                    if($row = $stmt->fetch()){
-                        $id = $row["UserID"];
-                        $username = $row["UserName"];
-                        $hashed_password = $row["Password"];
-
-                        //php built in verify function
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-                            echo "works";
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
+            if($password == $hashed_password){
+                // Store data in session variables
+                $_SESSION["loggedin"] = true;
+                $_SESSION["id"] = $id;
+                $_SESSION["username"] = $username;
+                // Redirect user to welcome page
+                header("location: welcome.php");
+            }else{
+                // Display an error message if password is not valid
+                $password_err = "The password you entered was not valid.";
             }
-
-            // Close statement
-            unset($stmt);
         }
-    }
 
-    // Close connection
-    unset($pdo);
+        if($count!=1){
+            // Display an error message if username doesn't exist
+            $username_err = "No account found with that username.";
+        }      
+    }
 }
 ?>
 
@@ -110,14 +87,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                  <!-- username form to recieve input -->
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
+                <span class="help-block"><?php if(ISSET($username_err)) {echo $username_err;} ?></span>
             </div>
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
 
                  <!-- password form to recieve input -->
                 <label>Password</label>
                 <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
+                <span class="help-block"><?php if(isset($password_err))echo $password_err; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
