5c8c3299ce6ef51347e05c151fcfe4e1a01f2f5f
euantaylor77 <eqtaylor@dundee.ac.uk>
2021-01-26 13:31:22 +0000

    added echo to check working

--- Software_Code/login.php
+++ Software_Code/login.php
@@ -4,7 +4,7 @@
 session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true){
     header("location: welcome.php");
     exit;
 }
@@ -63,7 +63,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
-
+                            echo "works"
                             // Redirect user to welcome page
                             header("location: welcome.php");
                         } else{
