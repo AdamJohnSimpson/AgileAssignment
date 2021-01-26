b2d06a601f532075cb41acd0c8918fca49b10a52
euantaylor77 <eqtaylor@dundee.ac.uk>
2021-01-26 15:50:23 +0000

    added comments

--- Software_Code/UserManagement/CreateUser.php
+++ Software_Code/UserManagement/CreateUser.php
@@ -41,11 +41,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
       }
 
-
+    //if both passwords are not the same
     if ($password != $confirm_password){
       echo "Passwords entered do not match";
     }
 
+    //if both passwords are the same and username is not used
     if($password == $confirm_password && $result['username'] != $username){
       // Prepare an insert statement
        $sql = "INSERT INTO Users (FirstName, Surname, EmailAddress, Role, UserName,  Password) VALUES ($firstname, $surname, $email, $role, $username, $password)";
@@ -72,38 +73,45 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <p>Please fill this form to create an account.</p>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
+              <!-- form to recieve first name from user -->
               <div class="form-group">
                   <label>First Name</label>
                   <input type="text" name="firstname" class="form-control" value="<?php echo $firstname; ?>">
               </div>
 
+              <!-- form to recieve surname from user -->
               <div class="form-group">
                   <label>Surname</label>
                   <input type="text" name="surname" class="form-control" value="<?php echo $surname; ?>">
               </div>
 
+              <!-- form to recieve email address from user -->
               <div class="form-group">
                   <label>Email Address</label>
                   <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
               </div>
 
+              <!-- form to recieve role from user -->
               <div class="form-group">
                   <label>Role</label>
                   <input type="text" name="role" class="form-control" value="<?php echo $role; ?>">
               </div>
 
+              <!-- form to recieve username from user -->
                 <div class="form-group ">
                     <label>Username</label>
                     <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
                     <span class="help-block"><?php if(isset($username_err))echo $username_err; ?></span>
                 </div>
 
+                <!-- form to recieve password from user -->
                 <div class="form-group ">
                     <label>Password</label>
                     <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
                     <span class="help-block"><?php if(isset($password_err))echo $password_err; ?></span>
                 </div>
 
+                <!-- form to confirm password from user -->
                 <div class="form-group ">
                     <label>Confirm Password</label>
                     <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
