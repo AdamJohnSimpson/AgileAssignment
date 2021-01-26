a5e8c0979e48c5cfe3133e75d66182cf392c0bee
euantaylor77 <eqtaylor@dundee.ac.uk>
2021-01-26 13:40:50 +0000

    added brackets

--- Software_Code/login.php
+++ Software_Code/login.php
@@ -6,7 +6,7 @@ session_start();
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
-    exit;
+    exit();
 }
 
 // Include database file
