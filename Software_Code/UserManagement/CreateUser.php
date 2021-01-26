709010572db4c72408723368ea5c33fd74b1a092
euantaylor77 <eqtaylor@dundee.ac.uk>
2021-01-26 16:24:59 +0000

    grammar fix

--- Software_Code/UserManagement/CreateUser.php
+++ Software_Code/UserManagement/CreateUser.php
@@ -17,14 +17,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
+        $username_err = "Please enter a username.";
     } else{
         $username = trim($_POST["username"]);
     }
 
     // Check if password is empty
     if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
+        $password_err = "Please enter a password.";
     } else{
         $password = trim($_POST["password"]);
     }
