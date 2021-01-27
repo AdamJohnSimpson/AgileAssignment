1f41ba5fe6161c96f2c80d23cc49a6a4daa99819
scottfulton2 <43143379+scottfulton2@users.noreply.github.com>
2021-01-27 14:25:36 +0000

    included comment to come back to, to remind me what other features I'm still to implement

--- Software_Code/experimentCreate.php
+++ Software_Code/experimentCreate.php
@@ -36,13 +36,21 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
     <div class="jumbotron" style="margin-bottom:1px;">
       <form>
         <div class="form-group">
-          <label>Enter Name of experiment</label>
+          <label>Enter Name of Experiment</label>
           <input type="text" name="Name of Experiment"><br><br>
           <!--check if the experiment name already exists and display error message to user?-->
+          <!--store input from user in session variable and compare it to result from SQL query to the database-->
+      </form>
+      <br></br>
+      <form>
+        <div class="form-group">
+          <label>Enter Description of experiment</label>
+          <input type="text" name="Description of Experiment"><br><br>
       </form>
       <br></br>
       <form>
         <input type="submit" value="Submit For Approval">
+        <!-- -->
       </form>
     </div>
   </div>
