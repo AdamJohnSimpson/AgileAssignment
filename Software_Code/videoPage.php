0a1caa9153ae99a3c838f621e3223fc91cbdedf5
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2021-01-27 15:10:45 +0000

    fixed issue with . and ..

--- Software_Code/videoPage.php
+++ Software_Code/videoPage.php
@@ -25,17 +25,23 @@
         // $experiment = $_SESSION['experimentid'];
         $experiment = "1";
         $allVideos = scandir("videos/" . $experiment . "/");
+
+        if (($key = array_search('.', $allVideos)) !== false) {
+          unset($array[$key]);
+        }
+        if (($key = array_search('..', $allVideos)) !== false) {
+          unset($array[$key]);
+        }
+
         for ($x=0; $x < count($allVideos); $x++) {
           $path = "videos/" . $experiment . "/" . $allVideos[$x];
           echo $path;
-          if ($allVideos[$x] != "." || $allVideos[$x] != "..") {
             echo "
             <br>
             <video src='" . $path . "' width='320' height='240' type='video/mp4' controls autoplay>
               Your browser does not support the video tag.
             </video>
             <br><br>";
-          }
         }
 
        ?>
