ffb8bcdec4df8fc4430a7dd21f621cc2fa50c138
euantaylor77 <eqtaylor@dundee.ac.uk>
2021-01-27 10:51:25 +0000

    added template

--- Software_Code/UserManagement/CreateUser.php
+++ Software_Code/UserManagement/CreateUser.php
@@ -113,16 +113,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
   <!DOCTYPE html>
   <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>User Management</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-        <style type="text/css">
-            body{ font: 14px sans-serif; }
-            .wrapper{ width: 350px; padding: 20px; }
-        </style>
-    </head>
+
+  <head>
+    <meta charset="utf-8">
+    <title>User Management</title <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+  </head>
+
     <body>
+
+      <header>
+        <img class="img-fluid" src="University-of-Dundee-logo.png" width="300px" style="padding:20px">
+      </header>
+
+      <div class="jumbotron text-center">
+        <h1 class="text-center">Template Heading</h1>
+      </div>
+
+      <div class="container-fluid" style="padding:0">
+        <div class="jumbotron" style="margin-bottom:1px;">
+
         <div class="wrapper">
             <h2>Create Users</h2>
             <p>Please fill this form to create an account.</p>
@@ -179,7 +192,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" value="Submit">
-
                 </div>
+
+                <footer>
+                      <img class="img-fluid mx-auto d-block" src="University-of-Dundee-logo-small.png" width="100px" style="padding:20px">
+                </footer>
+
       </body>
+
     </html>
