a029ff8124fcc3cc34c62a54f66bbe8002aef4d3
scottfulton2 <43143379+scottfulton2@users.noreply.github.com>
2021-01-27 10:18:15 +0000

    added the start of displaying information

--- Software_Code/experimentInformation.php
+++ Software_Code/experimentInformation.php
@@ -1,4 +1,13 @@
 <?php
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
+  header("location: login.php");
+  exit;
+}
+
+//make sure correct user is logged in for the experiment they are accessing
+
+//get information from experiment list page to display the selected experiment
+
 
 
 ?>
@@ -29,13 +38,9 @@
     <div class="jumbotron" style="margin-bottom:1px;">
       <form>
         <div class="form-group">
-          <label>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</label>
-          <input type="text" name="question"><br><br>
-          <input type="submit" value="Template button">
-      </form>
-      <br></br>
-      <form>
-        <input type="submit" value="Template button">
+          <label>Information</label> <!-- get information from expeirment table -->
+          <input type="text" name="add info">
+          <input type="submit" value="Edit Information">
       </form>
     </div>
   </div>
