832dbc5610f05bbd7970e3403b58974f98925f68
betho <bethogilvy4@gmail.com>
2021-01-27 08:55:12 +0000

    fixed wrong wording

--- Software_Code/makeQuestionnaires.php
+++ Software_Code/makeQuestionnaires.php
@@ -115,9 +115,8 @@
       </div>
     <div class="container-fluid" style="padding:0">
       <div class="jumbotron" style="margin-bottom:1px;">
-        <h2 class="text-center">Questionnaire name is:
+        <h2 class="text-center">You are creating a questionnaire for experiment:
         <?php echo $_SESSION['experimentName']; ?></h2>
-        <?php echo "<h2> ".$_SESSION['experimentName']." </h2>"; ?>
           <form method="POST">
             <div class="form-group">
               <label>Please enter the name of the questionnaire you are creating : </label>
