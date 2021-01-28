085883b39f210753de9ab5d79fd561fe944b798c
Craig <craig.ritchie450@gmail.com>
2021-01-28 09:56:23 +0000

    Created a page to thank participants for taking part in a questionnaire

--- Software_Code/ThankYou.php
+++ Software_Code/ThankYou.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Error</title <!-- Bootstrap CSS -->
+  <title>Thank You</title <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
@@ -12,16 +12,16 @@
 
 <body>
   <header>
-    <img class="img-fluid" src="../University-of-Dundee-logo.png" width="300px" style="padding:20px">
+    <img class="img-fluid" src="University-of-Dundee-logo.png" width="300px" style="padding:20px">
   </header>
 
   <div class="jumbotron text-center">
-    <h1 class="text-center">Error 404</h1>
-    <p>The page you are looking for doesn't exist</p>
+    <h1 class="text-center">Thank You!</h1>
+    <p>Thanks for taking part in our questionnaire</p>
   </div>
 
   <footer>
-        <img class="img-fluid mx-auto d-block" src="../University-of-Dundee-logo-small.png" width="100px" style="padding:20px">
+        <img class="img-fluid mx-auto d-block" src="University-of-Dundee-logo-small.png" width="100px" style="padding:20px">
   </footer>
 </body>
 
