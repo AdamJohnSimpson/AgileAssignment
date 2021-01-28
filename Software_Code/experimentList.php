469604ef18c00dafb1107a067388a5d44ddd9e7e
AdamJohnSimpson <adamj911gmail.com>
2021-01-28 10:47:19 +0000

    fixed php error

--- Software_Code/experimentList.php
+++ Software_Code/experimentList.php
@@ -56,7 +56,7 @@
              </div>
            </div>";
         }
-        echo "<a href='experimentCreate.php'> <button class='btn btn-outline-success' type='button'>Create new experiment</button> </a>"
+        echo "<a href='experimentCreate.php'> <button class='btn btn-outline-success' type='button'>Create new experiment</button> </a>";
         if(isset($_GET['i']) && isset($_GET['n']) && isset($_GET['r']))
         {
             func($_GET['i'], $_GET['n'], $_GET['r']);
