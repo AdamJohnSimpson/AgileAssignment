187a8c55b500f9fe205a788d2ce1422832414042
Holly Groves <holly.groves1912@gmail.com>
2021-01-28 10:40:29 +0000

    got my lefts and rights the proper way around :))))))

--- Software_Code/experimentCreate.php
+++ Software_Code/experimentCreate.php
@@ -80,9 +80,9 @@ if(isset($_POST['logout'])) {
 
 <body>
   <header>
-    <img class="img-fluid" src="University-of-Dundee-logo.png" width="300px" style="padding:20px; float: left">
+    <img class="img-fluid" src="University-of-Dundee-logo.png" width="300px" style="margin:20px; float: left">
     <form method="POST">
-      <input type="submit" value="Log Out" name="logout" style="float: left; padding:20px">
+      <input type="submit" value="Log Out" name="logout" style="float: right; margin:20px">
     </form>
   </header>
 
