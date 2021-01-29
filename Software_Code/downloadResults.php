26b888d7c0d277f6d4945d4546f932440c1216ac
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2021-01-29 09:40:42 +0000

    Initial code to export to CSV

---
+++ Software_Code/downloadResults.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+// Database Connection
+require_once "Includes/db.inc.php";
+
+// get Users
+$query = "SELECT * FROM users";
+if (!$result = mysqli_query($con, $query)) {
+    exit(mysqli_error($con));
+}
+
+$users = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $users[] = $row;
+    }
+}
+
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=Users.csv');
+$output = fopen('php://output', 'w');
+fputcsv($output, array('No', 'First Name', 'Last Name', 'Email'));
+
+if (count($users) > 0) {
+    foreach ($users as $row) {
+        fputcsv($output, $row);
+    }
+}
+
+?>
