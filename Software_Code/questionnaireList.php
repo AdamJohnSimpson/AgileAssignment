d858436a31aabc6f5b6a10a4dad0faf82b27dc9c
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2021-02-01 15:37:45 +0000

    Added view results button

--- Software_Code/questionnaireList.php
+++ Software_Code/questionnaireList.php
@@ -60,6 +60,7 @@ $tempURL = "https://agile-assignment-group-4.azurewebsites.net/Questionnaire.php
               $questionnaireURL = $tempURL.$questionnaireID;
               echo " <p>".$questionnaireURL."</p>
               <br>
+              <a href='https://agile-assignment-group-4.azurewebsites.net/viewQuestionnaireResults.php?qid={$questionnaireID}'> <button class='btn btn-outline-success' type='button'>View Results</button> </a>
               <a href='https://agile-assignment-group-4.azurewebsites.net/downloadResults.php?qid={$questionnaireID}'> <button class='btn btn-outline-success' type='button'>Dowload Results</button> </a>
              </div>
            </div>";
