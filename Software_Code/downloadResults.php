0e35585b5ddbc6d009914715aacdad0c28c4b6a6
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2021-02-01 15:26:15 +0000

    removed unecessary code and finishing touches

--- Software_Code/downloadResults.php
+++ Software_Code/downloadResults.php
@@ -49,11 +49,7 @@ fputcsv($output, array('questionID', 'questionText', 'responseID', 'response'));
 if (count($listOfQuestionID) > 0) {
     for ($x=0; $x < count($listOfQuestionID); $x++) {
       for ($y=0; $y < count($allResults[$x]); $y++) {
-        // $row = array($listOfQuestionID[$x], $listOfQuestionText[$x], $allResultID[$x][$y], $allResults[$x][$y]);
         fputcsv($output, array($listOfQuestionID[$x], $listOfQuestionText[$x], $allResultID[$x][$y], $allResults[$x][$y]));
-        // fputcsv($output, array(99,99,$x,$y));
-
-        // fputcsv($output, $row);
       }
     }
 }
