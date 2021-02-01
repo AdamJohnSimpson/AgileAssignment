fa8087518021e64aa21cab8302ed30cf5de4aaec
Holly Groves <holly.groves1912@gmail.com>
2021-02-01 11:48:18 +0000

    fixed the title so it doesn't show template anymore

--- Software_Code/individualResponse.php
+++ Software_Code/individualResponse.php
@@ -10,7 +10,7 @@ require_once "Includes/db.inc.php";
 
 <head>
   <meta charset="utf-8">
-  <title>Template</title>
+  <title>Individual Responses</title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
