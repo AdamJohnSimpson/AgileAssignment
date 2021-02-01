b5844c2a3a82cfada34e553481be73752e777bb9
Holly Groves <holly.groves1912@gmail.com>
2021-02-01 15:10:49 +0000

    wrote sql queries to get the response for a specific question

--- Software_Code/individualResponse.php
+++ Software_Code/individualResponse.php
@@ -45,6 +45,12 @@ $responseID = "60181209f21d8"; //hard coded until page is finished and can be li
          echo "<div class='row'>
            <div class='card-body'>
             <h5 class='card-text mt-2'>".$questionTxt."</h5>";
+            $stmt = "SELECT * FROM results WHERE questionID = '{$questionID}' AND responseID = '{$responseID}'"; //get the response for the question it is on
+            $resultResponse = mysqli_query($conn, $stmt);
+            while($row = mysqli_fetch_array($resultResponse)){
+
+            }
+
 
 
 
