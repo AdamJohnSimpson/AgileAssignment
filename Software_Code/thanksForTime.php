02a2194a1e3530990b839688d79d38bcd046a363
euantaylor77 <eqtaylor@dundee.ac.uk>
2021-02-01 15:34:35 +0000

    created thanks for time page

---
+++ Software_Code/thanksForTime.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Thank You</title> <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
+  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="https://www.dundee.ac.uk/themes/custom/uod/assets/favicons/favicon.ico"/>
+</head>
+
+<body>
+  <header>
+    <img class="img-fluid" src="University-of-Dundee-logo.png" width="300px" style="padding:20px">
+  </header>
+
+  <div class="jumbotron text-center">
+    <h1 class="text-center">Thank you for your time.</h1>
+    <p>Sorry we could not be of more help, we hope you can enjoy the questionnaires in future.</p>
+  </div>
+
+  <footer>
+        <img class="img-fluid mx-auto d-block" src="University-of-Dundee-logo-small.png" width="100px" style="padding:20px">
+  </footer>
+</body>
+
+</html>
