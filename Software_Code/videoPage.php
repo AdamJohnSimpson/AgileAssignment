579cec0ba9d1843c1dff6d807f3d1f80a1a50c86
scottfulton2 <43143379+scottfulton2@users.noreply.github.com>
2021-02-01 16:03:37 +0000

    changed colour of button for going to timestamp

--- Software_Code/videoPage.php
+++ Software_Code/videoPage.php
@@ -62,12 +62,12 @@ if(isset($_POST['logout'])) {
   </div>
   <br></br>
   <div class="jumbotron text-center">
-    <h2 class="text-centre">"Timestamp 1"</h2>
+    <h2 class="text-centre">Timestamp 1</h2>
     <br></br>
     <p>This is the description for timestamp 1</p>
   </div>
   <form>
-    <input type="button" value="Go to Timestamp 1" name="Timestamp1">
+    <input type="button" class="btn btn-primary" value="Go to Timestamp 1" name="Timestamp1">
   </form>
   <form action="https://agile-assignment-group-4.azurewebsites.net/experimentInformation.php">
       <input type="submit" value="Return to Experiments Information" />
