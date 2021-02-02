77365fb70efbf9bc9181827f0d1602f37fec277d
Craig <craig.ritchie450@gmail.com>
2021-02-02 11:13:12 +0000

    Added page to add co-researchers

--- Software_Code/UserManagement/ManageCoResearchers.php
+++ Software_Code/UserManagement/ManageCoResearchers.php
@@ -98,7 +98,7 @@
 							echo "<td>" . $row['UserName'] . "</td>";
 							echo "<td>" . $row['Firstname'] . "</td>";
 							echo "<td>" . $row['Surname'] . "</td>";
-							echo '<td> <button onclick="location.href=\''.$link.'&aid=' .$row['UserID'] .'\';" type="button" class="btn btn-secondary">Add</button> </td>';
+							echo '<td> <button onclick="location.href=\'Includes/addco.inc.php?eid=' .$eID .'&aid='.$row['UserID'].'\'" type="button" class="btn btn-secondary">Add</button> </td>';
 
 							echo "</tr>";
 
