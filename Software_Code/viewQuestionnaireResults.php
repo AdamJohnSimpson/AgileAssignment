1ae9b181d3179bac4f227db0e1352d85bcfea259
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2021-02-02 10:25:46 +0000

    Added in test bar chart and styling

--- Software_Code/viewQuestionnaireResults.php
+++ Software_Code/viewQuestionnaireResults.php
@@ -86,6 +86,21 @@ for ($x=0; $x < count($listOfQuestionText) ; $x++) {
               }
             } else {
               echo "<h3>{$listOfQuestionType[$x]}</h3>";
+
+              <div class="chart-wrap vertical">
+              <h2 class="title">Test Title</h2>
+
+              <div class="grid">
+                  <div class="bar" style="--bar-value:85%;" data-name="Your Blog" title="Your Blog 85%"></div>
+                  <div class="bar" style="--bar-value:23%;" data-name="Medium" title="Medium 23%"></div>
+                  <div class="bar" style="--bar-value:7%;" data-name="Tumblr" title="Tumblr 7%"></div>
+                  <div class="bar" style="--bar-value:38%;" data-name="Facebook" title="Facebook 38%"></div>
+                  <div class="bar" style="--bar-value:35%;" data-name="YouTube" title="YouTube 35%"></div>
+                  <div class="bar" style="--bar-value:30%;" data-name="LinkedIn" title="LinkedIn 30%"></div>
+                  <div class="bar" style="--bar-value:5%;" data-name="Twitter" title="Twitter 5%"></div>
+                  <div class="bar" style="--bar-value:20%;" data-name="Other" title="Other 20%"></div>
+              </div>
+            </div>
             }
           }
         } else {
