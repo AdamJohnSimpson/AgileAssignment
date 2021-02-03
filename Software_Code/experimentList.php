988982acc35de2148db3f2240bf19cd2f0448f54
Craig <craig.ritchie450@gmail.com>
2021-02-03 15:46:24 +0000

    Added semicolon

--- Software_Code/experimentList.php
+++ Software_Code/experimentList.php
@@ -26,7 +26,7 @@
 
     <header style="height:150px;">
       <a href="Includes/redirect.inc.php"><img class="img-fluid" src="University-of-Dundee-logo.png" width="300px" style="padding:20px; float: left"></a>
-      <button onclick="location.href='Includes/logout.inc.php'" type='button' class='btn btn-secondary' style="float: right; margin:20px">Logout</button>
+      <button onclick="location.href='Includes/logout.inc.php';" type='button' class='btn btn-secondary' style="float: right; margin:20px">Logout</button>
 
     </header>
 
