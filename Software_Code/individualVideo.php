d86fc11043eb9369c35c67110465281e70fdaf25
AdamJohnSimpson <adamj911gmail.com>
2021-02-03 14:33:39 +0000

    formatting

--- Software_Code/individualVideo.php
+++ Software_Code/individualVideo.php
@@ -42,7 +42,6 @@ $videoPath = $_GET['p'];
       <?php
         //get information from experiment list page to display the selected experiment
         $query = "SELECT * FROM videos WHERE videoAddress= '$videoPath'";
-        echo "sql = SELECT * FROM videos WHERE videoAddress= {$videoPath}";
         $result = mysqli_query($conn, $query);
         // foreach( $result as $row ) {
         while($row = mysqli_fetch_array($result)){
