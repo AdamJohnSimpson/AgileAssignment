34c5c69e39f04930a225667c8f78617ba257c921
AdamJohnSimpson <adamj911gmail.com>
2021-02-04 10:34:17 +0000

    added testing

--- Software_Code/videoEdit.php
+++ Software_Code/videoEdit.php
@@ -76,7 +76,7 @@ if(isset($_POST['editDesc'])){
   </header>
 
   <div class="jumbotron text-center">
-    <h1 class="text-center">Edit video details</h1>
+    <h1 class="text-center">Edit video details hi!</h1>
   </div>
   <div class="container-fluid" style="padding:0">
     <div class="jumbotron" style="margin-bottom:1px;">
